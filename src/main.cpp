a2c722b0fe421915b0ebdf92e14a12320dabe4a2
Tanmay Singhal <tanmay.singhal_cs.aiml24@gla.ac.in>
2026-02-20 10:09:07 +0530

    Implement simple servo rotation
    Added comments and documentation for servo rotation.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,28 @@
 #include <Arduino.h>
+#include <Servo.h>
+
+/**
+ * @file main.cpp
+ * @brief Simple Servo Rotation Example
+ * @author TANMAY SINGHAL
+ * @date 2026-02-17
+ *
+ * @details
+ * Rotates servo motor to 90 degree and 180 degree.
+ */
+
+Servo myServo;
+int servoPin = 6;
+
 void setup() {
-// write your initialization code here
+    myServo.attach(servoPin);
 }
 
-
 void loop() {
-// write your code here
+    myServo.write(90);
+    delay(2000);
+
+    myServo.write(180);
+    delay(2000);
+
 }
